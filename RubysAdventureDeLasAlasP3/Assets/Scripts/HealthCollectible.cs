db68070dc5c4a4bd38e6c219c6039042a8d7a72e
Zoey Joiz De Las Alas <arrazoe@gmail.com>
2023-11-10 23:23:07 -0800

    Decorated the map more, added more scripts, added enemies and damage zones
    Added house, edited map, added damageable zone, created a script for DZ, added enemies, created script for enemies, edited collectible prefab and script

--- RubysAdventureDeLasAlasP3/Assets/Scripts/HealthCollectible.cs
+++ RubysAdventureDeLasAlasP3/Assets/Scripts/HealthCollectible.cs
@@ -7,6 +7,16 @@ public class HealthCollectible : MonoBehaviour
     // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Object that entered the trigger: " + other);
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+            if (controller.health < controller.maxHealth)
+                {
+                    controller.ChangeHealth(1);
+                    Destroy(gameObject);
+                }
+           
+        }
     }
 }
--- RubysAdventureDeLasAlasP3/Assets/Scripts/RubyController.cs
+++ RubysAdventureDeLasAlasP3/Assets/Scripts/RubyController.cs
@@ -8,18 +8,29 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
     
     public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+    
+    public int health { get { return currentHealth; } }
     int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
     
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
+
     // Start is called before the first frame update
     void Start()
     {
-         rigidbody2d = GetComponent<Rigidbody2D>();
-
+        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         currentHealth = maxHealth;
+        
     }
 
     // Update is called once per frame
@@ -28,9 +39,26 @@ public class RubyController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2 (horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        if(isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if(invincibleTimer < 0 )
+                isInvincible = false;
+        }
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
         position.x = position.x + speed * horizontal * Time.deltaTime;
@@ -38,8 +66,18 @@ public class RubyController : MonoBehaviour
         rigidbody2d.MovePosition(position);
     }
 
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            animator.SetTrigger("Hit");
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+            
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
