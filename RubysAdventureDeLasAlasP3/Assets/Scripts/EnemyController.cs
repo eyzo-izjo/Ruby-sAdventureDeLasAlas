4d31c37bb55590cb53e8a112879fb9eaa898f000
Zoey Joiz De Las Alas <arrazoe@gmail.com>
2023-11-13 18:49:47 -0800

    Added projectiles, edited enemy animation
    Added projectile cog bullet, made it a prefab, and gave it a script, edited enemy animations to work, added fixed animations, added Character and Projectile layer

--- RubysAdventureDeLasAlasP3/Assets/Scripts/EnemyController.cs
+++ RubysAdventureDeLasAlasP3/Assets/Scripts/EnemyController.cs
@@ -11,6 +11,7 @@ public class EnemyController : MonoBehaviour
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
+    bool broken = true;
 
     Animator animator;
 
@@ -25,6 +26,11 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!broken)
+        {
+            return;
+        }
+
         timer -= Time.deltaTime;
 
         if(timer < 0)
@@ -35,6 +41,11 @@ public class EnemyController : MonoBehaviour
     }
     void FixedUpdate()
     {
+        if (!broken)
+        {
+            return;
+        }
+
         Vector2 position = rigidbody2D.position;
 
         if (vertical)
@@ -61,5 +72,12 @@ public class EnemyController : MonoBehaviour
         {
             player.ChangeHealth(-1);
         }
+    }
+
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
+        animator.SetTrigger("Fixed");
     }
 }
--- RubysAdventureDeLasAlasP3/Assets/Scripts/RubyController.cs
+++ RubysAdventureDeLasAlasP3/Assets/Scripts/RubyController.cs
@@ -6,10 +6,11 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
-    
+
     public int maxHealth = 5;
+    public GameObject projectilePrefab;
     public float timeInvincible = 2.0f;
-    
+
     public int health { get { return currentHealth; } }
     int currentHealth;
 
@@ -19,18 +20,19 @@ public class RubyController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
-    
+
     Animator animator;
-    Vector2 lookDirection = new Vector2(1,0);
+    Vector2 lookDirection = new Vector2(1, 0);
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
         rigidbody2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         currentHealth = maxHealth;
-        
+
     }
 
     // Update is called once per frame
@@ -39,9 +41,9 @@ public class RubyController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
-        Vector2 move = new Vector2 (horizontal, vertical);
+        Vector2 move = new Vector2(horizontal, vertical);
 
-        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
         {
             lookDirection.Set(move.x, move.y);
             lookDirection.Normalize();
@@ -50,12 +52,19 @@ public class RubyController : MonoBehaviour
         animator.SetFloat("Look Y", lookDirection.y);
         animator.SetFloat("Speed", move.magnitude);
 
-        if(isInvincible)
+        if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
-            if(invincibleTimer < 0 )
+            if (invincibleTimer < 0)
                 isInvincible = false;
         }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
+
+
     }
 
     void FixedUpdate()
@@ -76,9 +85,20 @@ public class RubyController : MonoBehaviour
 
             isInvincible = true;
             invincibleTimer = timeInvincible;
-            
+
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
+    }
 }
+   
