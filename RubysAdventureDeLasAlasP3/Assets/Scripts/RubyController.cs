f525e30d12a6db2e089643a679f2456c9e2a777f
Zoey Joiz De Las Alas <arrazoe@gmail.com>
2023-11-22 23:59:21 -0800

    Created NPC and added Dialog Raycast
    Added Jambi NPC, created new NPC layer, created NPC script, added dialog raycast so on click X in front of Jambi, dialog appears

--- RubysAdventureDeLasAlasP3/Assets/Scripts/RubyController.cs
+++ RubysAdventureDeLasAlasP3/Assets/Scripts/RubyController.cs
@@ -64,6 +64,18 @@ public class RubyController : MonoBehaviour
             Launch();
         }
 
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if(hit.collider != null)
+            {
+               NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
+        }
 
     }
 
@@ -88,7 +100,7 @@ public class RubyController : MonoBehaviour
 
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
 
     void Launch()
@@ -100,5 +112,7 @@ public class RubyController : MonoBehaviour
 
         animator.SetTrigger("Launch");
     }
+
+    
 }
    
